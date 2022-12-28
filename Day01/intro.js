0a183a701b9f8a1001476617eb32323559a6d94a
Ritika Ranjan <rranjan1922@gmail.com>
2022-12-28 06:58:49 +0530

    Initial commit

---
+++ Day01/intro.js
@@ -0,0 +1,6 @@
+//Day 1 of Web Dev
+//Printing
+console.log("Introduction to JavaScript");
+// defining a variable
+let variable = "I am a variable";
+console.log("The value of the variable is:",variable);
